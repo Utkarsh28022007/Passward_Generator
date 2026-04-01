0ec68d208d5dd25b1d544948ab3af21a360a8672
Utkarsh28022007 <utkarshq0q@gmail.com>
2026-04-01 18:19:23 +0530

    Fix typo in password generation function name

--- passward_generator.py
+++ passward_generator.py
@@ -1,7 +1,7 @@
 import random
 import string
 
-def generate_password(min_length,numbers=True,special_character=True):
+def generote_password(min_length,numbers=True,special_character=True):
     letters = string.ascii_letters
     digits= string.digits 
     special= string.punctuation
